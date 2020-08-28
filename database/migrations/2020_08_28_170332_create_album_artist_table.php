ae7dbacdba9a118dc7527a9b79dc6602592fb75e
Kadae-Aung <kadae-aung@Kadae-Aungs-MacBook-Pro.local>
2020-08-29 01:13:50 +0630

    kadae

---
+++ database/migrations/2020_08_28_170332_create_album_artist_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAlbumArtistTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('album_artist', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('album_id')
+                  ->references('id')->on('albums')
+                  ->onDelete('cascade');
+            $table->foreignId('artist_id')
+                  ->references('id')->on('artists')
+                  ->onDelete('cascade');
+            $table->softDeletes();
+            $table->timestamps();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('_album__artist');
+    }
+}
